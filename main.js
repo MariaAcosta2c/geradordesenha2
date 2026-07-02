ca5378d2c033afde2ac3e5e56304f68ea94e3592
MariaAcosta2c <maria.acosta.silva@escola.pr.gov.br>
2026-07-02 07:53:19 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,9 +5,14 @@ numeroSenha.textContentn = tamanhoSenha;
 const botoes = document.querySelectorAll('.parametro-senha__botao')
 
 botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho
 
 function diminuiTamanho(){
     tamanhoSenha = tamanhoSenha-1;
     numeroSenha.textContentn = tamanhoSenha;
+}
+function aumentaTamanho(){
+   tamanhoSenha = tamanhoSenha+1;
+   numeroSenha.textoContent = tamanhoSenha
 }
 console.log(botoes);
